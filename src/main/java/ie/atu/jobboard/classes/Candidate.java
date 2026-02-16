149c2227c7105899542c4696123cf8ee07a13b28
limzhilit <g00440629@atu.ie>
2026-02-16 11:10:30 +0000

    init and candidate class

---
+++ src/main/java/ie/atu/jobboard/classes/Candidate.java
@@ -0,0 +1,12 @@
+package ie.atu.jobboard.classes;
+
+public class Candidate {
+  private String name;
+  private String email;
+  private String phone;
+  private String experience;
+  private String skills;
+  private String location;
+  private String age;
+
+}
---
+++ src/test/java/ie/atu/jobboard/JobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
