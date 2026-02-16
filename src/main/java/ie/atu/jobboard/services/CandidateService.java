1c8960b2dd2b7f18f5515840cc2b208900bd323f
limzhilit <g00440629@atu.ie>
2026-02-16 11:18:56 +0000

    candidate controller and service

---
+++ src/main/java/ie/atu/jobboard/services/CandidateService.java
@@ -0,0 +1,11 @@
+package ie.atu.jobboard.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CandidateService {
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+}
